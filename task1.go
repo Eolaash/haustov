c54adbb08ed985873782973f4fb130da156df13d
Eolaash <blackoutat@mail.ru>
2021-02-17 15:37:22 +0400

    task 1 main func ready

--- task1.go
+++ task1.go
@@ -6,3 +6,24 @@ import "fmt"
 func TestTask() {
 	fmt.Println("HELLO!")
 }
+
+// SolutionSquareGenerator - generate int array of SQ of naturalnumbers
+func SolutionSquareGenerator(start int, n int) []int {
+
+	// logic preventers (overload preveneter? maxint*maxint)
+	if n < 1 || start < 1 {
+		tOutArray := make([]int, 0, 0)
+
+		// fail return
+		return tOutArray
+	}
+
+	// blank array
+	tOutArray := make([]int, n, n)
+	for tIndex := range tOutArray {
+		tOutArray[tIndex] = (start + tIndex) * (start + tIndex)
+	}
+
+	// success return
+	return tOutArray
+}
