e3e22322ae203bbafca8b2fc142685b4474dd2df
Eolaash <blackoutat@mail.ru>
2021-02-17 16:45:29 +0400

    task 2 added

---
+++ task2.go
@@ -0,0 +1,30 @@
+package haustov
+
+import "fmt"
+
+// SolutionBinaryGap - searching longes zero-stray in binary view of N
+func SolutionBinaryGap(N int) int {
+
+	// Prepare work vars
+	tMaxStray := 0
+	tCurrentStray := 0
+	tBinaryString := fmt.Sprintf("%b", N)
+
+	// Main scanner
+	for tIndex := range tBinaryString {
+		if tBinaryString[tIndex] == 0 {
+			tCurrentStray++
+		} else {
+			tMaxStray = tCurrentStray
+			tCurrentStray = 0
+		}
+	}
+
+	// Finalyzer
+	if tCurrentStray > tMaxStray {
+		tMaxStray = tCurrentStray
+	}
+
+	// succes return
+	return tMaxStray
+}
